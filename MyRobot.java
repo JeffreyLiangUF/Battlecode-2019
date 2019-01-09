243079d05768e70a4d6cd534c55bb9709dde4d5a
JeffreyLiangUF <jeffreyliang@ufl.edu>
2019-01-09 01:28:30 -0500

    build pilgrim

--- MyRobot.java
+++ MyRobot.java
@@ -4,27 +4,39 @@ public class MyRobot extends BCAbstractRobot {
 	public int turn;
 
     public Action turn() {
-				turn++;
+    	turn++;
+
 		
+    	if (me.unit == SPECS.CASTLE) {
+		    			log("Building a pilgrim.");
+								    			log("Building a pilgrim.");
+		    			log("Building a pilgrim.");
+		    			log("Building a pilgrim.");
+		    			log("Building a pilgrim.");
+		    			log("Building a pilgrim.");
+		    			log("Building a pilgrim.");
 
-			//DELETED
+    		if (turn == 1) {
+    			log("Building a pilgrim.");
+    			return buildUnit(SPECS.PILGRIM,1,0);
+    		}
+    	}
 
-    	//Hello
+    	if (me.unit == SPECS.PILGRIM) {
+		    			log("Building a pilgrim.");
+
+    		if (turn == 1) {
+    			log("I am a pilgrim.");
+                 
+                //log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
+    		}
+    	}
 
     	return null;
-			//HI Jeff.
-			//fuuuuuuk
+
 	}
+	
 
 
-/*class Castle extends BCAbstractRobot{
-	MyRobot myRobot;
-
-    public Castle(MyRobot myRobot){
-        this.myRobot = myRobot;
-    }*/
-
-    public void act(){
-		log("Test");
-    }
+	
 }
