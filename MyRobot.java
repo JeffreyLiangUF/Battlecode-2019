b58dfdd8bee98efcb109a4049d24029aee148d65
Rafe Kruse <rafekruse01@gmail.com>
2019-01-08 23:25:03 -0500

    First Commit

---
+++ MyRobot.java
@@ -0,0 +1,42 @@
+package bc19;
+
+public class MyRobot extends BCAbstractRobot {
+	public int turn;
+
+    public Action turn() {
+				turn++;
+		
+
+    	if (me.unit == SPECS.CASTLE) {
+    		if (turn == 1) {
+    			log("Building a pilgrim.");
+    			
+				act();
+    		}
+    	}
+
+    	if (me.unit == SPECS.PILGRIM) {
+
+    		if (turn == 1) {
+    			log("I am a pilgrim.");
+                 
+                //log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
+    		}
+    	}
+
+    	return null;
+
+	}
+
+
+/*class Castle extends BCAbstractRobot{
+	MyRobot myRobot;
+
+    public Castle(MyRobot myRobot){
+        this.myRobot = myRobot;
+    }*/
+
+    public void act(){
+		log("Test");
+    }
+}
