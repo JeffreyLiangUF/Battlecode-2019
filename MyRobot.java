77e9f0c11713d1964b2221e1db9efaf0f7e60d43
Rafe Kruse <rafekruse01@gmail.com>
2019-01-09 01:29:55 -0500

    rafe

--- MyRobot.java
+++ MyRobot.java
@@ -8,35 +8,16 @@ public class MyRobot extends BCAbstractRobot {
 
 		
     	if (me.unit == SPECS.CASTLE) {
-		    			log("Building a pilgrim.");
-								    			log("Building a pilgrim.");
-		    			log("Building a pilgrim.");
-		    			log("Building a pilgrim.");
-		    			log("Building a pilgrim.");
-		    			log("Building a pilgrim.");
-		    			log("Building a pilgrim.");
-
     		if (turn == 1) {
-    			log("Building a pilgrim.");
     			return buildUnit(SPECS.PILGRIM,1,0);
     		}
     	}
 
     	if (me.unit == SPECS.PILGRIM) {
-		    			log("Building a pilgrim.");
-
     		if (turn == 1) {
-    			log("I am a pilgrim.");
-                 
-                //log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
+    			log("I am a pilgrim.");                 
     		}
     	}
-
-    	return null;
-
-	}
-	
-
-
-	
+		return null;	
+	}	
 }
