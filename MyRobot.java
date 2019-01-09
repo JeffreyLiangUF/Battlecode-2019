44de197523475f9b0a29654ad757da1ff86d99db
Rafe Kruse <rafekruse01@gmail.com>
2019-01-09 01:44:33 -0500

    New

--- MyRobot.java
+++ MyRobot.java
@@ -2,22 +2,39 @@ package bc19;
 
 public class MyRobot extends BCAbstractRobot {
 	public int turn;
+	//not enirely sure how this turn thing works. Aswell as how to preform multiple actions
 
     public Action turn() {
     	turn++;
 
 		
     	if (me.unit == SPECS.CASTLE) {
-    		if (turn == 1) {
-    			return buildUnit(SPECS.PILGRIM,1,0);
-    		}
+			Castle.Execute();
+			//Will have to include more because you can take more than one action per turn
     	}
 
     	if (me.unit == SPECS.PILGRIM) {
-    		if (turn == 1) {
-    			log("I asdfalgrim.");                 
-    		}
+    		Pilgrim.Execute();
     	}
 		return null;	
 	}	
 }
+
+
+
+class Castle extends BCAbstractRobot{
+
+	public static Action Execute(){
+		return buildUnit(SPECS.PILGRIM,1,0);
+	}
+
+}
+
+class Pilgrim extends BCAbstractRobot{
+
+	public static Action Execute(){
+		return buildUnit(SPECS.PILGRIM,1,0);
+	}
+
+}
+
