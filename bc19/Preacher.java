9b58cb1f2e6cedab9db76d975466981f39ae1624
JeffreyLiangUF <jeffreyliang@ufl.edu>
2019-01-13 19:13:04 -0500

    working on defend

--- bc19/Preacher.java
+++ bc19/Preacher.java
@@ -1,4 +1,5 @@
 package bc19;
+import java.util.ArrayList;
 
 public class Preacher extends MovingRobot implements Machine{
 	
@@ -7,6 +8,8 @@ public class Preacher extends MovingRobot implements Machine{
 	int ourTeam; //red:0 blue:1
 	Position location;
 	boolean mapIsHorizontal;
+	ArrayList<Position> castlePositions;
+	Position closestCastle;
  
 	public Preacher(MyRobot robot)
 	{
@@ -22,7 +25,7 @@ public class Preacher extends MovingRobot implements Machine{
         ourTeam = robot.me.team == robot.SPECS.RED ? 0 : 1;
 		mapIsHorizontal = Helper.FindSymmetry(robot.map);
 		location = new Position(robot.me.y, robot.me.x);
-		
+		GetClosestCastle();
     }
 
 	public Action AttackClosest()
@@ -43,5 +46,43 @@ public class Preacher extends MovingRobot implements Machine{
 			}
 		}
 		return robot.attack(visibleRobots[closestIndex].y - location.y, visibleRobots[closestIndex].x - location.x);	
+	}
+
+	public Action MoveToDefend()
+	{
+		Position enemyCastle = Helper.FindEnemyCastle(robot.map, mapIsHorizontal, closestCastle);
+		float distFromCastleToCastle = Helper.DistanceSquared(closestCastle, enemyCastle);
+		int movespeed = robot.SPECS.UNITS[robot.me.unit].SPEED;
+		int visionRange = (int)Math.sqrt(robot.SPECS.UNITS[robot.me.unit].VISION_RADIUS);
+
+		for (int i = -visionRange; i < visionRange; i++)
+		{
+			for (int j = -visionRange; j < visionRange; j++)
+			{
+				Position defenceTile = new Position (location.y + i, location.x + j);
+				float distFromTileToEnemyCastle = Helper.DistanceSquared(defenceTile, enemyCastle);
+						
+				if (distFromTileToEnemyCastle < distFromCastleToCastle)
+				{
+					
+				}
+			}
+		}
+
+	}
+
+	void GetClosestCastle()
+	{
+		Position closestCastle = null;
+		float least = Integer.MAX_VALUE;
+		for (Position castlePos : castlePositions)
+		{
+			float distance = Helper.DistanceSquared(castlePos, location);
+			if (distance < least)
+			{
+				least = distance;
+				closestCastle = castlePos;
+			}
+		}
 	}
 }
