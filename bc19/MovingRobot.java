7ec5a1abab9faf2fcdf46481f0cebfcddfec4b43
JeffreyLiangUF <jeffreyliang@ufl.edu>
2019-01-15 11:56:59 -0500

    watchforsignal

--- bc19/MovingRobot.java
+++ bc19/MovingRobot.java
@@ -379,6 +379,19 @@ public class MovingRobot {
 		}
 		return MoveCloser(robot, enemyCastle);
 	}
+
+	boolean WatchForSignal(MyRobot robot, int signal)
+	{
+		Robot[] robots = robot.getVisibleRobots();
+		for (int i = 0; i < robots.length; i++)
+		{
+			if (robots[i].signal == signal)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 }
 
 class PathingPosition {
